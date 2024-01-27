1a0a55963f9a4f355f9a7738aeecc3b1b3f81d21
Reajasharii <150302335+reajasharii@users.noreply.github.com>
2024-01-27 02:26:53 +0100

    ,

--- Login.php
+++ Login.php
@@ -1,3 +1,6 @@
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,21 +21,17 @@
 <div class="login">
     <div class="loginForm">
         <h1>Log in</h1>
-        <span id="passwordError" class="error-message"></span>
+        <span id="Error" class="error-message"></span>
      <form onsubmit="return validateForm()">
         <div class="forma">
-            <input type="text" id="username" placeholder="Username" required>
+            <input type="text" id="username" placeholder="Username" >
         </div>
         <div class="forma">
-            <input type="password" id="password" placeholder="Password" required>
-            <span id="passwordError" class="error-message"></span>
-            <style>
-                .error-message {
-                    color: red; 
-                    font-size: 12px; 
-                    
-                }
-            </style>
+            <input type="password" id="password" placeholder="Password">
+           <style> .error-message {
+                  color: red; 
+                   font-size: 15px;
+                   font-weight: bold;}</style>
         </div>
         <div class="remember-forgot">
             <label><input type="checkbox">  Remeber me</label>
@@ -49,42 +48,7 @@
 <?php
 include 'footer.php';
 ?>
-   
-
-    
-    <script>
-   
-        function validateForm() {
-            var username = document.getElementById("username").value;
-            var password = document.getElementById("password").value;
-            var passwordError = document.getElementById('passwordError');
-       
-    
-            if (username.trim() === "") {
-                passwordError.innerHTML = "";
-                alert("please write username");
-                document.getElementById('username').focus();
-                return false;
-            }
-
-            if (password.length < 8) {
-                passwordError.innerHTML = "the password must be longer than 8 characters";
-                document.getElementById('password').focus();
-                return false;
-            }
-
-            if (!password.match(/[a-zA-Z]/)) {
-                passwordError.innerHTML = "the password must contain an alphabetic character";
-                document.getElementById('password').focus();
-                return false;
-            }
-         
-       alert("You are logged in!");
-      
-             return true;
-    }
-    </script>
-    
+    <script type ="text/javascript" src="script/login.js"></script>
     
 </body>
 </html>
--- signup.php
+++ signup.php
@@ -1,38 +1,3 @@
-<?php
-$email= "";
-$username="";
-$password="";
-$dateBirth="";
-$Gender="";
-
-$errorMessage="";
-$succesMessage="";
-
-if($_SERVER['REQUEST_METHOD']=='POST'){
-    if (isset($_POST['submit'])){
-
-    $email= $_POST ["email"];
-    $username= $_POST["username"];
-    $password=$_POST["password"];
-    $dateBirth=$_POST["dateBirth"];
-    $Gender=$_POST["Gender"];
-
-    do{
-        if(empty($email) || empty($username)|| empty($password) || empty($dateBirth)|| empty($Gender)){
-            $errorMessage = "All the fiels are required";
-            break;
-        }
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $errorMessage = "Please enter a valid email address";
-            break;
-        }
-
-        $successMessage = "sign up correctly";
-    }while(false);
-}
-}
-?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -55,67 +20,28 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     <div class="loginForm">
         <h1>Sign up</h1>
 
-        <?php
-        if(!empty($errorMessage)){
-            echo "
-            <div class='alert alertError' role='alert'>
-                <strong>$errorMessage</strong>
-                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
-            </div>";
-        }
-        ?>
-     
- 
-        <form method="post" action="skripti_regjistrimit.php" onsubmit="return validateForm()">
-
-
-        
+      <form onsubmit="return validateForm()">
+      <span id="Error" class="error-message"></span>
         <div class="forma">
-            <input type="email" id="email" name="email" placeholder="Email" required value="<?php echo $email; ?>">
-            <span id="emailError" class="error-message"></span>
-         
-        </div>
-        <div class="forma">
-            <input type="text" id="username" name="username" placeholder="Username" required value="<?php echo $username; ?>">
-        </div>
-        <div class="forma"> 
-            <input type="password" id="password" name="password" placeholder="Password" required value="<?php echo $password; ?>">
-            <span id="passwordError" class="error-message"></span>
-            <style>
-                .error-message {
-                    color: red; 
-                    font-size: 12px; 
-                    
-                }
-            </style>
+            <input type="email" id="email" name="email" placeholder="Email">
             
         </div>
         <div class="forma">
-                <input type="date" id="dob" name="dateBirth" required value="<?php echo $dateBirth; ?>">
-            </div>
-        <div class="forma">
-                <label for="gender">Gender:</label>
-                <select id="gender" name="Gender" required value="<?php echo $Gender; ?>">
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                    <option value="other">Other</option>
-                </select>
-            </div>
+            <input type="text" id="username" name="username" placeholder="Username">
+            
+        </div>
+        <div class="forma"> 
+            <input type="password" id="password" name="password" placeholder="Password">
+           
+            <style> .error-message {
+                  color: red; 
+                   font-size: 15px;
+                   font-weight: bold;}</style>
+        </div>
+    
         <div class="remember-forgot">
             <label><input type="checkbox"> Remeber me</label>
-          
         </div>
-
-        <?php
-        if(!empty($succesMessage)){
-            echo "
-            <div class='alert alertSuccess' role='alert'>
-                <strong>$successMessage</strong>
-                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
-            </div>";
-    }
-        
-        ?>
         <button type="submit" class="butoni">Signup</button>
 
         <div class="fundi">
@@ -128,43 +54,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 <?php
 include 'footer.php';
 ?>
-   
+   <script type ="text/javascript" src="script/signup.js"></script>
 
-    <script>
-        function validateForm() {
-            var email = document.getElementById("email").value;
-            var username = document.getElementById("username").value;
-            var password = document.getElementById("password").value;
-            var passwordError = document.getElementById('passwordError');
-            var emailError = document.getElementById('emailError');
-    
-            if (username === "") {
-                alert("Username must be filled out");
-                return false;
-            }
-            var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-if (!emailRegex.test(email)) {
-    emailError.innerHTML = "Please enter a valid email address";
-    document.getElementById('email').focus();
-    return false;
-}
-
-          
-            if (password.length < 8) {
-                passwordError.innerHTML = "the password must be longer than 8 characters";
-                document.getElementById('password').focus();
-                return false;
-            }
-
-            if (!password.match(/[a-zA-Z]/)) {
-                passwordError.innerHTML = "the password must contain an alphabetic character";
-                document.getElementById('password').focus();
-                return false;
-            }
-         
-    
-            return true;
-        }
-    </script>
 </body>
 </html>
